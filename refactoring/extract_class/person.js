60d92bc46934f318309687f839a50c3894a03248
sanghwan <jae6120@naver.com>
2020-06-02 08:46:27 +0900

    적절한 네이밍 수정

--- refactoring/extract_class/person.js
+++ refactoring/extract_class/person.js
@@ -4,17 +4,17 @@ class Person {
   }
   get name() {return this._name;};
   set name(arg) {this._name = arg;};
-  get telephoneNumber() {return this._telephoneNumber.telephoneNumber;}
-  get officeAreaCode() {return this._telephoneNumber.officeAreaCode;}
-  set officeAreaCode(arg) {this._telephoneNumber.officeAreaCode = arg;}
-  get officeNumber() {return this._telephoneNumber.officeNumber;}
-  set officeNumber(arg) {this._telephoneNumber.officeNumber = arg;}
+  get telephoneNumber() {return this._telephoneNumber.toString();}
+  get officeAreaCode() {return this._telephoneNumber.areaCode;}
+  set officeAreaCode(arg) {this._telephoneNumber.areaCode = arg;}
+  get officeNumber() {return this._telephoneNumber.number;}
+  set officeNumber(arg) {this._telephoneNumber.number = arg;}
 }
 
 class TelephoneNumber {
-  get officeAreaCode() {return this._officeAreaCode;}
-  set officeAreaCode(arg) {this._officeAreaCode = arg;}
-  get officeNumber() {return this._officeNumber;}
-  set officeNumber(arg) {this._officeNumber = arg;}
-  get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`;}
+  get areaCode() {return this._areaCode;}
+  set areaCode(arg) {this._areaCode = arg;}
+  get number() {return this._number;}
+  set number(arg) {this._number = arg;}
+  get toString() {return `(${this.areaCode}) ${this.number}`;}
 }
