1a6906b5025ac3fe0dd132a17c2a08626312688c
sanghwan <jae6120@naver.com>
2020-07-08 19:47:09 +0900

    반복문 쪼개기 step1 반복문 복제

--- refactoring/split_roop/index.js
+++ refactoring/split_roop/index.js
@@ -5,4 +5,9 @@ for (const p of people) {
 	totalSalary += p.salary;
 }
 
+for (const p of people) {
+	if (p.age < youngest) youngest = p.age;
+	totalSalary += p.salary;
+}
+
 return `최연소 ${youngest}, 총 급여: ${totalSalary}`; 
