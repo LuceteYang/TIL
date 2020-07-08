f66224f1700e297117b496e7f7356ba7559df74a
sanghwan <jae6120@naver.com>
2020-07-08 19:50:23 +0900

    반복문 쪼개기 step3 함수로 추출

--- refactoring/split_roop/index.js
+++ refactoring/split_roop/index.js
@@ -1,11 +1,17 @@
-let youngest = people[0] ? people[0].age : Infinity;
-let totalSalary = 0;
-for (const p of people) {
-	totalSalary += p.salary;
+return `최연소 ${youngestAge()}, 총 급여: ${totalSalary()}`;
+
+function totalSalary() {
+	let totalSalary = 0;
+	for (const p of people) {
+		totalSalary += p.salary;
+	}
+	return totalSalary;
 }
 
-for (const p of people) {
-	if (p.age < youngest) youngest = p.age;
+function youngestAge() {
+	let youngest = people[0] ? people[0].age : Infinity;
+	for (const p of people) {
+		if (p.age < youngest) youngest = p.page;
+	}
+	return youngest
 }
-
-return `최연소 ${youngest}, 총 급여: ${totalSalary}`; 
