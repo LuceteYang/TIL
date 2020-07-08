ecdc65a846493ae8ef475b1d25f1b38608b453e9
sanghwan <jae6120@naver.com>
2020-07-08 19:51:25 +0900

    반복문 쪼개기 step4 반복문 파이프라인으로 바꿈

--- refactoring/split_roop/index.js
+++ refactoring/split_roop/index.js
@@ -1,17 +1,9 @@
 return `최연소 ${youngestAge()}, 총 급여: ${totalSalary()}`;
 
 function totalSalary() {
-	let totalSalary = 0;
-	for (const p of people) {
-		totalSalary += p.salary;
-	}
-	return totalSalary;
+   return people.reduce((total, p) => total + p.salary, 0);
 }
 
 function youngestAge() {
-	let youngest = people[0] ? people[0].age : Infinity;
-	for (const p of people) {
-		if (p.age < youngest) youngest = p.page;
-	}
-	return youngest
+   return Math.min(...people.map(p => p.age));
 }
