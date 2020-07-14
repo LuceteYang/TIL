a18809fc2a9520e77a43c86e555880a88e3f7c6f
sanghwan <jae6120@naver.com>
2020-07-15 05:49:51 +0900

    접근자 이름 수 정

--- refactoring/rename_field/index.js
+++ refactoring/rename_field/index.js
@@ -1,10 +1,10 @@
 class Organization {
   constructor(data) {
-    this._title = (data.title !== undefiend) ? data.title: data.name;
+    this._title = data.title = data.title;
     this._country = data.country;
   }
-  get name() {return this._title;}
-  set name(aString) {this._title = aString;}
+  get title() {return this._title;}
+  set title(aString) {this._title = aString;}
 
   get country() {return this._country;}
   set country(aCountryCode) {this._country = aCountryCode;}
