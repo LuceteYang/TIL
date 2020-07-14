80158967ab530f0fae5fc957fc246c3ca98651fa
sanghwan <jae6120@naver.com>
2020-07-15 05:48:43 +0900

    객체 안의 private 필드명을 변경

--- refactoring/rename_field/index.js
+++ refactoring/rename_field/index.js
@@ -1,10 +1,10 @@
 class Organization {
   constructor(data) {
-    this._name = data.name;
+    this._title = data.name;
     this._country = data.country;
   }
-  get name() {return this._name;}
-  set name(aString) {this._name = aString;}
+  get name() {return this._title;}
+  set name(aString) {this._title = aString;}
 
   get country() {return this._country;}
   set country(aCountryCode) {this._country = aCountryCode;}
