4350ff45738d85d1aedcf7bf71bafc11dca5abd4
sanghwan <jae6120@naver.com>
2020-11-26 11:55:08 +0900

    10 - 06 세터 메서드 추가

--- refactoring/introduce_assertion/index.js
+++ refactoring/introduce_assertion/index.js
@@ -1,11 +1,19 @@
 class Customer {
-  discountRate: number;
+  _discountRate: number;
+
+  set discountRate(aNumber) {
+    this._discountRate = aNumber;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
 
   applyDiscount(aNumber) {
-    if (!this.discountRate) return aNumber;
+    if (!this._discountRate) return aNumber;
     else {
-      assert(this.discountRate >= 0);
-      return aNumber - (this.discountRate) * aNumber;
+      assert(this._discountRate >= 0);
+      return aNumber - (this._discountRate) * aNumber;
     } 
   }
 }
