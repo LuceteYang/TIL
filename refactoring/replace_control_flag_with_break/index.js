3b14ba50e2ef64f74364a30f69ea85d2b06d6d16
sanghwan <jae6120@naver.com>
2020-11-26 12:18:59 +0900

    10 - 07 제어 플래그 참조하는 다른 코드 모두 제거

--- refactoring/replace_control_flag_with_break/index.js
+++ refactoring/replace_control_flag_with_break/index.js
@@ -6,17 +6,14 @@ function someFunction(){
 }
 
 function checkForMiscreants(people) {
-  let found = false;
   for (const p of people) {
-    if(!found) {
-      if (p === "조커") {
-        sendAlert();
-        return;
-      }
-      if (p === "사루만") {
-        sendAlert();
-        found = true;
-      }
+    if (p === "조커") {
+      sendAlert();
+      return;
+    }
+    if (p === "사루만") {
+      sendAlert();
+      return;
     }
   }
 }
