670aefe4f8f6db00df13abc1655473f0d4668f9f
sanghwan <jae6120@naver.com>
2020-11-26 11:49:39 +0900

    10 - 06 어서션 추가

--- refactoring/introduce_assertion/index.js
+++ refactoring/introduce_assertion/index.js
@@ -4,6 +4,7 @@ class Customer {
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
     else {
+      assert(this.discountRate >= 0);
       return aNumber - (this.discountRate) * aNumber;
     } 
   }
